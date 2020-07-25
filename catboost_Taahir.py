5d960af9cb8ac99176edae624ed766be52d187fa
TaahirBhorat <53185076+TaahirBhorat@users.noreply.github.com>
2020-07-25 13:27:17 +0200

    Update catboost_Taahir.py

--- catboost_Taahir.py
+++ catboost_Taahir.py
@@ -57,6 +57,18 @@ def add_weather(trips_df, weather_df):
     df = pd.merge(trips_df, weather_df, how='left', on='date').drop('date', axis=1)
     return df
 
+def haversine_array(lat1, lng1, lat2, lng2):
+    lat1, lng1, lat2, lng2 = map(np.radians, (lat1, lng1, lat2, lng2))
+    AVG_EARTH_RADIUS = 6371  # in km
+    lat = lat2 - lat1
+    lng = lng2 - lng1
+    d = np.sin(lat * 0.5) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(lng * 0.5) ** 2
+    h = 2 * AVG_EARTH_RADIUS * np.arcsin(np.sqrt(d))
+    return h
+
+train.loc[:, 'distance_haversine'] = haversine_array(train['Origin_lat'].values, train['Origin_lon'].values, train['Destination_lat'].values, train['Destination_lon'].values)
+
+
 
 def clean_training_set(trips_df):
     # remove trips with average speed over 200
