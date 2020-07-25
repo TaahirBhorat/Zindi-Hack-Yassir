ef0a93d6555f4e0aeff07e888ba65403d904bad3
Stuart Mesham <stumes3@gmail.com>
2020-07-25 10:43:04 +0200

    added weather data loading

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,12 @@
 import pandas as pd
-df = pd.read_csv('Train.csv')
+trips_df = pd.read_csv('Train.csv')
 weather_df = pd.read_csv('Weather.csv')
 
-df['Timestamp'] = pd.to_datetime(df['Timestamp'], infer_datetime_format=True)
+trips_df['Timestamp'] = pd.to_datetime(trips_df['Timestamp'], infer_datetime_format=True)
 weather_df['date'] = pd.to_datetime(weather_df['date'], infer_datetime_format=True)
+
+trips_df['date'] = trips_df['Timestamp'].dt.date
+weather_df['date'] = weather_df['date'].dt.date
+
+df = pd.merge(trips_df, weather_df, how='left', on='date').drop('date', axis=1)
+del weather_df, trips_df
